5d15ceeea6327e840bb8e6736ecf90b229c5b0ae
Paul Cretu <paul.s.cretu@gmail.com>
2014-09-19 21:36:53 +0000

    Added default xcode files

---
+++ Recall/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Recall
+//
+//  Created by Paul Cretu on 9/14/14.
+//  Copyright (c) 2014 Paul Cretu. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Recall/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Recall
+//
+//  Created by Paul Cretu on 9/14/14.
+//  Copyright (c) 2014 Paul Cretu. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
